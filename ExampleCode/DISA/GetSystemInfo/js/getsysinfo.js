4b5096dddd10e79e65d57ee3321237373e9e7e73
OracleSiebel <duncan.ford@oracle.com>
2016-04-28 11:19:53 +0100

    Tidying
    Fixed indentation. Altered SiebelJS.Log to SiebelJD.Debug. Spelling
    fixes.

--- ExampleCode/DISA/GetSystemInfo/js/getsysinfo.js
+++ ExampleCode/DISA/GetSystemInfo/js/getsysinfo.js
@@ -2,7 +2,7 @@
 // so that the Component Type and Version could be picked up by DISA Framework
 // Note: the following block only needs to execute once before the first call to send message to DISA.
 var consts = SiebelJS.Dependency("SiebelApp.Constants");
-consts.set("WS_COMPONENT_TYPE_SYSINFO","plugin_sysinfo");
+consts.set("WS_COMPONENT_TYPE_SYSINFO", "plugin_sysinfo");
 consts.set("WS_PLUGIN_SYSINFO_VERSION", "1.0.0");
 
 // Create message handler and implement its interface.
@@ -11,45 +11,46 @@ consts.set("WS_PLUGIN_SYSINFO_VERSION", "1.0.0");
 var sysinfoHandler = null;
 
 // This sends a message to DISA
-// The entire sctructure of the message is something for the developer to decide, but it must be a JSON object
+// The entire structure of the message is something for the developer to decide, but it must be a JSON object
+// or Blob object.
 // In this case, the assumption is that the Java code will expect an object with a field called Command having a value
 // of GetSysInfo
-// Neither "Command" or "GetSysInfo" are DISA-specific, this is just how our plusing example expects the input
+// Neither "Command" or "GetSysInfo" are DISA-specific, this is just how our plugin example expects the input
 //     --- if (msg.get(COMMAND).getAsString().equals("GetSysInfo")) {
 function getSystemInformationFromDISA() {
-    var handler = getSysInfoHandler();
-    var msgJSON = {};
-    msgJSON["Command"] = "GetSysInfo";
-    handler.SendMessage(msgJSON);
+	var handler = getSysInfoHandler();
+	var msgJSON = {};
+	msgJSON["Command"] = "GetSysInfo";
+	handler.SendMessage(msgJSON);
 }
 
 // Here we instantiate a WebSocketHandler and define event callbacks
 function getSysInfoHandler() {
-    if (sysinfoHandler === null) {
-        sysinfoHandler = SiebelApp.WebSocketManager.CreateWSHandler(consts.get("WS_COMPONENT_TYPE_SYSINFO"));
+	if (sysinfoHandler === null) {
+		sysinfoHandler = SiebelApp.WebSocketManager.CreateWSHandler(consts.get("WS_COMPONENT_TYPE_SYSINFO"));
 
-        sysinfoHandler.OnClose = onWSClose;
-        sysinfoHandler.OnFail = onSendFail;
-        sysinfoHandler.OnMessage = onWSMessage;
-    }
+		sysinfoHandler.OnClose = onWSClose;
+		sysinfoHandler.OnFail = onSendFail;
+		sysinfoHandler.OnMessage = onWSMessage;
+	}
 
-    return sysinfoHandler;
+	return sysinfoHandler;
 }
 
 // This is where we process an incoming message from the DISA plugin, in this case expecting specific content from the sysinfo plugin
 function onWSMessage(msg) {
-    var hostAddr = msg["HostAddress"];
-    var hostName = msg["HostName"];
-    var processor = msg["Processor"];
-    var userhome = msg["UserHome"];
-    var content = "***System Information***\n"
-                + "Host IP Address:" + hostAddr + "\n"
-                + "Host Name:" + hostName + "\n"
-                + "Processor Count:" + processor + "\n"
-                + "User Home:" + userhome + "\n"
+	var hostAddr = msg["HostAddress"];
+	var hostName = msg["HostName"];
+	var processor = msg["Processor"];
+	var userhome = msg["UserHome"];
+	var content = "***System Information***\n"
+		 + "Host IP Address:" + hostAddr + "\n"
+		 + "Host Name:" + hostName + "\n"
+		 + "Processor Count:" + processor + "\n"
+		 + "User Home:" + userhome + "\n";
 
-    // Log the result to the console
-	SiebelJS.Log(content);
+	// Log the result to the console
+	SiebelJS.Debug(content);
 }
 
 // Normally this indicates something wrong with the communication attempt with DISA
@@ -57,7 +58,7 @@ function onWSMessage(msg) {
 // * maybe because the version number at two sides are not matched, operator version should be equal or newer
 // Reset state or other variables if necessary
 function onSendFail() {
-    SiebelJS.Debug("[DISA][Warning] Send message failed.");
+	SiebelJS.Debug("[DISA][Warning] Send message failed.");
 }
 
 // This indicates the Siebel OpenUI with DISA connection was lost
@@ -65,13 +66,13 @@ function onSendFail() {
 // * maybe because DISA exited (by user) or crashed
 // Reset state or other variables if necessary
 function onWSClose() {
-    SiebelJS.Debug("[DISA][Warning] DISA connection was closed.");
+	SiebelJS.Debug("[DISA][Warning] DISA connection was closed.");
 }
 
 // Call this function at the end of the pmodel's life
 function unregisterSysInfoHandler() {
-    if (sysinfoHandler) {
-        sysinfoHandler.Unregister();
-        sysinfoHandler = null;
-    }
+	if (sysinfoHandler) {
+		sysinfoHandler.Unregister();
+		sysinfoHandler = null;
+	}
 }
